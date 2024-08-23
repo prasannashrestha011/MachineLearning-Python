e4577623211f78e28e8f3dbe30ca319219c567c1
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-08-24 02:22:48 +0545

    working on scipy sparse matrix

--- main/scipy/scipy&numpy.py
+++ main/scipy/scipy&numpy.py
@@ -1,6 +1,12 @@
 from scipy import sparse
 import numpy as np
-eye=np.eye(5)
-print("original matrix: ",eye)
-sparse_matrix=sparse.csr_matrix(eye)
-print(sparse_matrix)
+#Defining the data with matrix containing ones
+eye=np.ones(4)
+row_indicies=np.arange(4)
+col_indicies=np.arange(4)
+#
+eye_coo=sparse.coo_matrix((eye,(row_indicies,col_indicies)))
+print(eye_coo)
+#convert the matrix into dense format
+eye_dense=eye_coo.toarray()
+print(eye_dense)
