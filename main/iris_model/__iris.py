c6b82b8a6c84bf86e5f90fe4a1adbce1a87115ce
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-08-25 16:46:09 +0545

    irish prediction model complete

--- main/iris_model/__iris.py
+++ main/iris_model/__iris.py
@@ -1,8 +1,12 @@
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+
+import numpy as np
 import pandas as pd
 import mglearn
 import matplotlib.pyplot as plt
+
 iris_dataset=load_iris()
 
 #displays all the data keys
@@ -32,4 +36,12 @@ print("X test data->{}".format(X_test.shape))
 iris_dataframe=pd.DataFrame(X_train,columns=iris_dataset.feature_names)
 grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
  hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
-plt.show()
+
+## encapsulating KNeighbors class algorithm in knn object by passing neighbor number=1
+knn=KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train,y_train)
+X_new=np.array([[5, 2.9, 1, 0.2]])
+y_predict=knn.predict(X_new)
+print("New predicition: {}".format(y_predict))
+print("New prediction name: {}".format(iris_dataset.target_names[y_predict]))
+
